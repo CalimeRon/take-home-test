b122a074be3c7cbb518a4134790642bc5581f4cc
Calimeron <ron.chamma@gmail.com>
2021-10-05 09:56:12 +0300

    add tests for herbal tea behavior

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -17,4 +17,24 @@ describe("Pharmacy - Regular drugs behavior", () => {
     );
   });
 });
+
+describe("Pharmacy - Herbal Tea behavior", () => {
+  it("should increase the benefit by 1 before expiration date", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 1, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 0, 1)]
+    );
+  });
+  it("should increase the benefit by 2 after expiration date", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 0, 0)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -1, 2)]
+    );
+  });
+  it("should not exceed a benefit of 50", () => {
+    expect(new Pharmacy([new Drug("Herbal Tea", 1, 50)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", 0, 50)]
+    );
+    expect(new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()).toEqual(
+      [new Drug("Herbal Tea", -1, 50)]
+    );
+  });
 });
