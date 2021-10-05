649c792df6e119af26d70eb610147229f86bf5b4
Calimeron <ron.chamma@gmail.com>
2021-10-05 10:00:31 +0300

    add tests for Magic Pill behavior

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,39 +2,50 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy - Regular drugs behavior", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", 1, 2)]);
   });
   it("should decrease the benefit twice as fast after expiration date", () => {
-    expect(new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", -1, 1)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -1, 1)]);
   });
   it("should not decrease the benefit below a zero value", () => {
-    expect(new Pharmacy([new Drug("test", 1, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 0, 0)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 1, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("test", 0, 0)]);
   });
 });
 
 describe("Pharmacy - Herbal Tea behavior", () => {
   it("should increase the benefit by 1 before expiration date", () => {
-    expect(new Pharmacy([new Drug("Herbal Tea", 1, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("Herbal Tea", 0, 1)]
-    );
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 1, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 0, 1)]);
   });
   it("should increase the benefit by 2 after expiration date", () => {
-    expect(new Pharmacy([new Drug("Herbal Tea", 0, 0)]).updateBenefitValue()).toEqual(
-      [new Drug("Herbal Tea", -1, 2)]
-    );
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -1, 2)]);
   });
   it("should not exceed a benefit of 50", () => {
-    expect(new Pharmacy([new Drug("Herbal Tea", 1, 50)]).updateBenefitValue()).toEqual(
-      [new Drug("Herbal Tea", 0, 50)]
-    );
-    expect(new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()).toEqual(
-      [new Drug("Herbal Tea", -1, 50)]
-    );
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 1, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 0, 50)]);
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 49)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -1, 50)]);
+  });
+});
+
+describe("Pharmacy - Magic Pill behavior", () => {
+  it("should never expire nor decrease in benefit", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 1, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 1, 0)]);
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 1, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 1, 50)]);
   });
 });
